7fe257713fb674ba47a569f65753c97e76128ed4
Guillermo Gallo <guillermo.gallo@getambassador.com>
2022-11-19 04:39:10 -0300

    Added test case for signing CSR

--- src/tests/CertificateManagementTest.php
+++ src/tests/CertificateManagementTest.php
@@ -17,24 +17,27 @@ use App\Services\TxtDnsRecordVerifier;
 
 class CertificateManagementTest extends TestCase
 {
+    protected CreateCertificateSigningRequest $createCertificateSigningRequest;
+    protected IDomainVerifier $domainVerifierService;
+
+    protected function setUp(): void
+    {
+        $this->createCertificateSigningRequest = new CreateCertificateSigningRequest();
+        $this->createCertificateSigningRequest->commonName = '*.ggallohernandez.com';
+        $this->createCertificateSigningRequest->organization = 'ggallohernandez';
+        $this->createCertificateSigningRequest->organizationalUnit = 'IT';
+        $this->createCertificateSigningRequest->city = 'Montevideo';
+        $this->createCertificateSigningRequest->state = 'Montevideo';
+        $this->createCertificateSigningRequest->country = 'UY';
+
+        $this->domainVerifierService = $this->createStub(IDomainVerifier::class);
+        $this->domainVerifierService->method('verify')->willReturn(true);
+    }
+
     public function testCreateANewCertificateSigningRequest()
     {
-        $domain = '*.ggallohernandez.com';
-        $validity_in_days = 365;
-        $csr = '';
-        
-        $request = new CreateCertificateSigningRequest();
-        $request->commonName = $domain;
-        $request->organization = 'ggallohernandez';
-        $request->organizationalUnit = 'IT';
-        $request->city = 'Montevideo';
-        $request->state = 'Montevideo';
-        $request->country = 'UY';
-
-        $domainVerifierService = $this->createStub(IDomainVerifier::class);
-        $domainVerifierService->method('verify')
-             ->willReturn(true);
-
+        $request = $this->createCertificateSigningRequest;
+        $domainVerifierService = $this->domainVerifierService;
 
         $certificateManager = new StepCaCertificateManager($domainVerifierService);
 
@@ -60,14 +63,32 @@ class CertificateManagementTest extends TestCase
         
         $request = new CreateCertificateRequest($domain, $validity_in_days, $csr);
 
-        $domainVerifierService = $this->createStub(IDomainVerifier::class);
-        $domainVerifierService->method('verify')
-             ->willReturn(true);
-
+        $domainVerifierService = $this->domainVerifierService;
 
         $certificateManager = new StepCaCertificateManager($domainVerifierService);
 
         $certificateService = new CertificateService($domainVerifierService, $certificateManager);
+
+        $certificate = $certificateService->createCertificate($request);
+
+        $this->assertEquals($request->commonName, $certificate->commonName);
+        $this->assertNotEmpty($certificate->content);
+    }
+
+    public function testCreateANewCertificateFromCSR()
+    {
+        $csrRequest = $this->createCertificateSigningRequest;
+        $domainVerifierService = $this->domainVerifierService;
+
+        $certificateManager = new StepCaCertificateManager($domainVerifierService);
+
+        $certificateService = new CertificateService($domainVerifierService, $certificateManager);
+
+        $csr = $certificateService->createCertificateSigningRequest($csrRequest);
+
+        $validity_in_days = 365;
+        
+        $request = new CreateCertificateRequest($csr->commonName, $validity_in_days, $csr->content);
 
         $certificate = $certificateService->createCertificate($request);
 
